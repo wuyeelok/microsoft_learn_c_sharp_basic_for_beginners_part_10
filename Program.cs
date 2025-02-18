c204a029282714f2934bdfe52158260ece40dfca
wuyeelok <wuyeelok@gmail.com>
2025-02-18 15:37:33 +0800

    counter without loop

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var part = 10;
 Console.WriteLine($"Hello, World! Part {part}");
+
+int counter = 0;
+
+// no loop
+counter = counter + 1;
+Console.WriteLine($"Counter is {counter}");
+counter = counter + 1;
+Console.WriteLine($"Counter is {counter}");
+counter = counter + 1;
+Console.WriteLine($"Counter is {counter}");
